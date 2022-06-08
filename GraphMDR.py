5dcebd322d00d46e350ac950e8a8fce30cfd7e7c
SA <sm.asadifar@gmail.com>
2022-06-08 01:27:52 -0700

    Update GraphMDR.py

--- GraphMDR.py
+++ GraphMDR.py
@@ -11,8 +11,16 @@ sys.path.insert(3, '/path/to/Sub-Questions-Generation/mainSQG')
 import mainGE as ge
 import setup as sr
 import mainSQG as sqg
-
-execfile('mainGE.py')
-execfile('setup.py')
 execfile('mainSQG.py')
+# Using readlines()
+file1 = open('myfile.txt', 'r')
+Lines = file1.readlines()
+count = 0
+# Strips the newline sub-question
+for line in Lines:
+  count += 1
+  execfile('mainGE.py')
+  execfile('setup.py')
+  
+
 
